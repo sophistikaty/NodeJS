002d96f3a767a2beeb72246a513db8dcab6ad7a0
sophistikaty <sophistikaty@me.com>
2015-01-16 16:41:57 -0500

    testing basics and text/port changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
-  res.writeHead(202, {'Content-Type': 'text/plain'});
+  res.writeHead(201, {'Content-Type': 'text/plain'});
   res.end('Hello World, I am programming in Node.JS!\n');
 }).listen(3000, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:3000/');
