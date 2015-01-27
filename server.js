9e58ca054d621488686fb7b4d22bbc38f4e6489c
sophistikaty <sophistikaty@me.com>
2015-01-27 17:20:01 -0500

    testing http basics/ status codes and redirects

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var http = require('http');
 http.createServer(function (req, res) {
-  res.writeHead(201, {'Content-Type': 'text/plain'});
-  res.end('Hello World, I am programming in Node.JS!\n');
+  // res.writeHead(200, {'Content-Type': 'text/plain'});
+  // res.end('Hello World, I am programming in Node.JS!\n');
+  res.writeHead(301, {
+  	'Location': 'http://philoticconnection.com/'
+  });
+  res.end();
 }).listen(3000, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:3000/');
